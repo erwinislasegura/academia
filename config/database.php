d7d11f13f892d5e92c195218a198747de7d67f3e
desarrollovolcan <eisla@fvolcan.cl>
2026-05-31 00:53:49 -0400

    db

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'driver' => 'mysql',
     'host' => getenv('DB_HOST') ?: '127.0.0.1',
     'port' => getenv('DB_PORT') ?: '3306',
-    'database' => getenv('DB_DATABASE') ?: 'academia_iquique',
+    'database' => getenv('DB_DATABASE') ?: 'academia',
     'username' => getenv('DB_USERNAME') ?: 'root',
     'password' => getenv('DB_PASSWORD') ?: '',
     'charset' => 'utf8mb4',
