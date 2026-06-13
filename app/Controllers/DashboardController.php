241c28750372abfe9009d36b0840fa773e5079e7
erwinislasegura <erwin.2785@gmail.com>
2026-06-13 15:58:31 -0400

    Redesign admissions KPI dashboard

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -6,12 +6,19 @@ final class DashboardController extends Controller
     {
         Middleware::permission('ver_dashboard');
         $userModel = new User();
+        $admissionModel = new AdmissionApplication();
         $this->view('dashboard/index', [
             'title' => 'Inicio Academiapp',
             'activeUsers' => $userModel->activeCount(),
             'rolesCount' => (new Role())->count(),
             'permissionsCount' => (new Permission())->count(),
-            'admissionsCount' => (new AdmissionApplication())->count(),
+            'admissionsCount' => $admissionModel->count(),
+            'admissionMetrics' => $admissionModel->dashboardMetrics(),
+            'applicationsByCourse' => $admissionModel->countByCourse(),
+            'applicationsByStatus' => $admissionModel->countByStatus(),
+            'applicationsByGender' => $admissionModel->countByGender(),
+            'applicationsByAgeRange' => $admissionModel->countByAgeRange(),
+            'latestApplications' => $admissionModel->latest(),
             'activity' => $userModel->recentActivity(),
         ]);
     }
